e7a53bc2be8fdf58439f2ec6ec67c0a827537533
Pyush-Patil <piyushpati1315@gmail.com>
2025-11-20 10:36:12 +0530

    Exploring GET POST and DELETE

--- src/app.js
+++ src/app.js
@@ -2,6 +2,12 @@ const express=require("express");
 
 const app=express();
 
+app.use("/",(req,res)=>{
+    res.send(console.log("namaste Piyush")
+    )
+});
+
+
 app.use("/test",(req,res)=>{
     res.send("Namaste from test");
 })  // Request Handlers
