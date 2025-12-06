1edc6ed070e7c5e70f53ebfb56305bbcab30e7e7
Pyush-Patil <piyushpati1315@gmail.com>
2025-12-06 12:16:18 +0530

    Playing with the Routes

--- src/app.js
+++ src/app.js
@@ -2,22 +2,41 @@ const express=require("express");
 
 const app=express();
 
-app.use("/",(req,res)=>{
-    res.send(console.log("namaste Piyush")
-    )
-});
+// app.use("/",(req,res)=>{
+//     res.send("Namaste Piyush")
+// });
 
+//This will one match to the GET APi call for /user
+app.get("/user/:UserID/:NAme/:Password",(req,res)=>{
+    console.log(req.params);
 
-app.use("/test",(req,res)=>{
-    res.send("Namaste from test");
-})  // Request Handlers
+    res.send([
+    {
+    Firstname:"Piyush",
+    lastname:"Patil",
+    Age:21,
+    },
+    {
+    Firstname:"John ",
+    lastname:"Doe",
+    Age:34
+    }
+])
+}
+) // Request Handlers
 
-app.use("/user",(req,res)=>{res.send("Hello from the users")}) // Request Handlers
+app.post("/user",(req,res)=>{
 
-app.use("/products",(req,res)=>{res.send("Hello from the product")}); // Request Handlers
+})
 
 app.listen(7777,()=>{
     console.log("Server is Successfully running on Port 7777");   
     
 });
 
+// if you write a*b this means you can write anything between a and b into your url and still i will run 
+//eg localhost:7777/aPIyUSHb
+
+// + this plus indicates
+
+///.*fly$/ this is regex which means your route mus end with fly 
---
+++ src/tempCodeRunnerFile.js
@@ -0,0 +1,3 @@
+app.use("/",(req,res)=>{
+//     res.send("Namaste Piyush")
+// });
