2888ba5c6e7b7f0ebe68ab25aa922d1bad42ef5e
Pyush-Patil <piyushpati1315@gmail.com>
2025-12-07 17:50:00 +0530

    Middlewares chain

--- src/app.js
+++ src/app.js
@@ -2,26 +2,27 @@ const express=require("express");
 
 const app=express();
 
-app.use("/user",[
-  (req,res,next)=>{
-  console.log("Handling Route User");
-  next(); //This will take you to the 2nd request handler 
-  // res.send("Response") // This will execute after the 2nd req handler func 
-  // The Error Comes because youre trying to send the response again after a successfull response alr sent over there 
-},
- (req,res,next)=>{
-  console.log("Handling Route User 2");
-  next();
-  // res.send("2nd response");
-},
-(req,res,next)=>{
-  console.log("Handling Route User 3");
-  next();
-   res.send("3rd response");
+app.use("/",(req,res,next)=>{
+  // res.send("Handling / route");
+   next(); // Remove or comment out if you want to send response
 }
-]
+);
+
+// /user has a chain of Middlewares=>request handlers
+app.get("/user",
+  (req,res,next)=>{
+     console.log(("Handling /user Route"));
+    next();
+  },
+  (req,res,next)=>{
+    res.send(" 2nd Route handler")
+    next();
+  },
+  (req,res,next)=>{
+    res.send(" 3rd Route handler")
+  }
 )
-//app.use("/route",[[rh1,rh2],rh3,rh4]) valid syntax
+
 
 app.listen(7777,()=>{
    console.log("Successfully Running on Port 7777");
