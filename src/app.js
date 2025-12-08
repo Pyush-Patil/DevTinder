84f36c24f49c9b7e911fe50c3458323de236c7d3
Pyush-Patil <piyushpati1315@gmail.com>
2025-12-08 19:52:48 +0530

    WildCard-Error-Handling

--- src/app.js
+++ src/app.js
@@ -1,36 +1,27 @@
+
 const express=require("express");
 
 const app=express();
 
-// Actual Use Case of Middleware 
-
-//Handling Auth Middleware for All GET POST PUT DELETE 
-// /admin will automatically authorize admin everytime when you try to do something as an admin
-
-const {adminauth,UserAuth}=require("./middlewares/auth")
-
-app.use("/admin",adminauth);
-
-app.get("/user",UserAuth,(req,res)=>{
-    res.send("Got the User")
+app.use("/getuserdata",(req,res)=>{
+  // try{
+    //logic to call db and get the user data 
+  throw new Error("Random error") // This code contains some error and  will throw along with some random code that will expose some of your data  
+  res.send("user data sent");
+  // }
+  // catch(err)
+  // {
+  //   res.status(501).send("Some Error occured Contact support team")
+  // }
 })
 
-app.post("/user/login",(req,res)=>{
-  //Logic to make the user login into your system
-  res.send("User Logged in Successfully");
+// A way handle the error gracefully
+app.use("/",(err,req,res,next)=>{
+  if(err)
+  {
+  res.status(500).send("Something went wrong")
+  }
 })
- 
-app.get("/admin/getAlldata",(req,res)=>{
-  //logic to send data
-  res.send("Sent All data")
-});
-
-app.get("/admin/deleteuser",(req,res)=>{
-     //logic to delete user
-     res.send("Deleted User");
-});
-
-
 
 
 app.listen(7777,()=>{
